78e6d144ec18655c18479c08691f3fceb488884f
aankcryzers <52010456+aankcryzers@users.noreply.github.com>
2025-06-24 19:45:51 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+self.addEventListener('install', event => {
+  self.skipWaiting(); // langsung aktif
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(keyList.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+});
